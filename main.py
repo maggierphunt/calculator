693e9d72082ad25f6e080576d181c45e136c8684
Maggie Hunt <maggie.hunt@paconsulting.com>
2022-11-24 01:50:27 +0000

    removing csv elements

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect
 
-#while writing to csv
-import csv
-import json
-
-
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])    
 def landing_page():
-    with open("Table.csv", "a", encoding="UTF8") as f:
-        writer = csv.writer(f, delimiter=",")
-        writer.writerow("")
 
     return render_template("index.html")
 
@@ -26,24 +18,6 @@ def calculator():
     inputNumber2=int(request.form['inputNumber2'])
     inputNumber3=int(request.form['inputNumber3'])
     total = inputNumber1+inputNumber2+inputNumber3
-
-    #when writing to csv
-
-    # adding row to csv
-    with open("Table.csv", "a", encoding="UTF8") as f:
-        writer = csv.writer(f, delimiter=",")
-
-        log_data1 = inputText1+","+str(inputNumber1)
-        log_data1 =  log_data1.split(", ")
-        writer.writerow(log_data1)
-
-        log_data2 = inputText2+","+str(inputNumber2)
-        log_data2 =  log_data2.split(", ")
-        writer.writerow(log_data2)
-
-        log_data3 = inputText3+","+str(inputNumber3)
-        log_data3 =  log_data3.split(", ")
-        writer.writerow(log_data3)
 
     print(total)
     return render_template("results.html",inputText1=inputText1, inputNumber1=inputNumber1, inputText2=inputText2, inputNumber2=inputNumber2, inputText3=inputText3, inputNumber3=inputNumber3,total=total)
