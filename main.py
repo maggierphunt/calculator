db03df3ecba4468b5ebd0bf399f2a12261934ca6
Maggie Hunt <maggie.hunt@paconsulting.com>
2022-11-24 03:41:14 +0000

    resolving bugs w sqlalchemy

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
+from operator import itemgetter
 import os
+from typing import ItemsView
 from flask import Flask, render_template, request, url_for, redirect
+import sqlalchemy
+from sqlalchemy import *
+from sqlalchemy.engine import create_engine
+from sqlalchemy.schema import *
+from pybigquery.api import ApiClient
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+import configparser
 
+parser = configparser.ConfigParser()
+parser.read(".env")
+
+project_id = parser.get("DEFAULT", "project_id")
+dataset_id = parser.get("DEFAULT", "dataset_id")
+credentials = parser.get("DEFAULT", "credentials")
+# table_name = parser.get("DEFAULT", "table")
+
+api_client = ApiClient()
+engine_path = "bigquery:///?DataSetId="+dataset_id+"&ProjectId="+project_id+"&InitiateOAuth=GETANDREFRESH&OAuthSettingsLocation="+credentials
+engine = create_engine(engine_path, arraysize=1000)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+# session.Model = session.make_declarative_base()
+# # base.query = session.query_property()
+# base = sqlalchemy.declarative_base()
+
+class products(ItemsView, price): #todo: sort
+    __tablename__ = "products"
+    item = Column(String,primary_key=True)
+    price = Column(Float)
 
 app = Flask(__name__)
 
@@ -18,11 +50,22 @@ def calculator():
     inputNumber2=int(request.form['inputNumber2'])
     inputNumber3=int(request.form['inputNumber3'])
     total = inputNumber1+inputNumber2+inputNumber3
+    print("Total: ", total)
+    
+    input1 = products(item=inputText1, price=inputNumber1)
+    input2 = products(item=inputText2, price=inputNumber2)
+    input3 = products(item=inputText3, price=inputNumber3)
 
-    print(total)
+    inputs_list=[input1, input2, input3]
+    print("List: ",inputs_list)
+    
+    for i in inputs_list:
+        session.add(i)
+        session.commit()
+        print("Committed, ", i)
+
+    
     return render_template("results.html",inputText1=inputText1, inputNumber1=inputNumber1, inputText2=inputText2, inputNumber2=inputNumber2, inputText3=inputText3, inputNumber3=inputNumber3,total=total)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
