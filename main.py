2cd66a09e1f507fcd6e4e021ba910c2381718b3e
Maggie Hunt <maggie.hunt@paconsulting.com>
2022-11-29 01:29:47 +0000

    front-end draft for deployment

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 from flask import Flask, render_template, request, url_for, redirect
-import os
-from operator import itemgetter
-from typing import ItemsView
-import sqlalchemy
-from sqlalchemy import *
-from sqlalchemy.engine import create_engine
-from sqlalchemy.schema import *
-from pybigquery.api import ApiClient
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
-import configparser
+# import os
+# from operator import itemgetter
+# from typing import ItemsView
+# import sqlalchemy
+# from sqlalchemy import *
+# from sqlalchemy.engine import create_engine
+# from sqlalchemy.schema import *
+# from pybigquery.api import ApiClient
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import relationship, sessionmaker
+# import configparser
 
-parser = configparser.ConfigParser()
-parser.read(".env")
-project_id = parser.get("DEFAULT", "project_id")
-dataset_id = parser.get("DEFAULT", "dataset_id")
-credentials = parser.get("DEFAULT", "credentials")
+# parser = configparser.ConfigParser()
+# parser.read(".env")
+# project_id = parser.get("DEFAULT", "project_id")
+# dataset_id = parser.get("DEFAULT", "dataset_id")
+# credentials = parser.get("DEFAULT", "credentials")
 
-api_client = ApiClient()
-engine_path = "bigquery:///?DataSetId="+dataset_id+"&ProjectId="+project_id+"&InitiateOAuth=GETANDREFRESH&OAuthSettingsLocation="+credentials
-engine = create_engine(engine_path, arraysize=1000)
-Session = sessionmaker(bind=engine)
-session = Session()
+# api_client = ApiClient()
+# engine_path = "bigquery:///?DataSetId="+dataset_id+"&ProjectId="+project_id+"&InitiateOAuth=GETANDREFRESH&OAuthSettingsLocation="+credentials
+# engine = create_engine(engine_path, arraysize=1000)
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
-base = declarative_base()
-class products(base):
-    __tablename__ = "calc_list.products"
-    item = Column(String,primary_key=True)
-    price = Column(Float)
+# base = declarative_base()
+# class products(base):
+#     __tablename__ = "calc_list.products"
+#     item = Column(String,primary_key=True)
+#     price = Column(Float)
 
 app = Flask("calculator")
 
@@ -46,17 +46,17 @@ def calculator():
     total = inputNumber1+inputNumber2+inputNumber3
     print("Total: ", total)
     
-    input1 = products(item=inputText1, price=inputNumber1)
-    input2 = products(item=inputText2, price=inputNumber2)
-    input3 = products(item=inputText3, price=inputNumber3)
+    # input1 = products(item=inputText1, price=inputNumber1)
+    # input2 = products(item=inputText2, price=inputNumber2)
+    # input3 = products(item=inputText3, price=inputNumber3)
 
-    inputs_list=[input1, input2, input3]
-    print("List: ",inputs_list)
+    # inputs_list=[input1, input2, input3]
+    # print("List: ",inputs_list)
 
-    for i in inputs_list:
-        session.add(i)
-        session.commit()
-        print("Committed, ", i)
+    # for i in inputs_list:
+    #     session.add(i)
+    #     session.commit()
+    #     print("Committed, ", i)
 
     return render_template("results.html",inputText1=inputText1, inputNumber1=inputNumber1, inputText2=inputText2, inputNumber2=inputNumber2, inputText3=inputText3, inputNumber3=inputNumber3,total=total)
 
